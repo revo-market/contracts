1f21074b5bbf7a9794284b84fb5f6371b1831fe5
Charlie Andrews <charlie.andrews@valoraapp.com>
2022-01-30 22:14:20 -0800

    WIP added mock bounty and staking rewards contracts
    need to see if MockStakingRewardsSingleToken can be swapped in for StakingRewards in a FarmBot instance despite not matching its bytecode...

---
+++ contracts/mocks/MockStakingRewards.sol
@@ -0,0 +1,79 @@
+pragma solidity ^0.8.0;
+
+import "../openzeppelin-solidity/contracts/IERC20.sol";
+import "../ubeswap-farming/contracts/StakingRewards.sol";
+
+contract MockStakingRewardsSingleToken is IStakingRewards {
+    IERC20 public rewardsToken;
+    IERC20 public stakingToken;
+    constructor(
+        address _rewardsToken,
+        address _stakingToken
+    ) {
+        rewardsToken = IERC20(rewardsToken);
+        stakingToken = IERC20(stakingToken);
+    }
+
+    // Views
+    function lastTimeRewardApplicable() external override view returns (uint256) {
+        // farm bot shouldn't need this
+        require(false);
+        return 0;
+    }
+
+    function rewardPerToken() external override view returns (uint256) {
+        // farm bot shouldn't need this
+        require(false);
+        return 0;
+    }
+
+    uint256 public amountEarned;
+    function setAmountEarned(uint256 _amountEarned) public {
+        amountEarned = _amountEarned;
+    }
+    function earned(address account) external override view returns (uint256) {
+        return amountEarned;
+    }
+
+    function getRewardForDuration() external override view returns (uint256) {
+        // farm bot shouldn't need this
+        require(false);
+        return 0;
+    }
+
+    function totalSupply() external override view returns (uint256) {
+        // farm bot shouldn't need this
+        require(false);
+        return 0;
+    }
+
+    uint256 public accountBalance;
+    function setAccountBalance(uint256 _accountBalance) external {
+        accountBalance = _accountBalance;
+    }
+    function balanceOf(address account) external override view returns (uint256) {
+        return accountBalance;
+    }
+
+    // Mutative
+    mapping(address => uint) public staked;
+    function stake(uint256 amount) external override {
+        staked[msg.sender] += amount;
+        stakingToken.transferFrom(msg.sender, address(this), amount);
+    }
+
+    function withdraw(uint256 amount) external override {
+        require(staked[msg.sender] >= amount);
+        staked[msg.sender] -= amount;
+        stakingToken.transfer(msg.sender, amount);
+    }
+
+    function getReward() external override {
+        rewardsToken.transfer(msg.sender, amountEarned);
+    }
+
+    function exit() external override {
+        // farm bot shouldn't need this
+        require(false);
+    }
+}
