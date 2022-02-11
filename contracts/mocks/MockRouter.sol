dad5f6916ef5000d904ecf53f33600f1625d9ddd
cajubelt <cajubelt@users.noreply.github.com>
2022-02-10 21:25:09 -0800

    test: farm bot tests (#10)
    * added typing on fee only bounty contract
    
    * added mock router contract
    
    * renamed mock staking rewards contract for clarity
    
    * WIP
    
    * Fix tests, add MockMoolaStakingRewards
    
    * revert deploy
    
    * revert farm bot
    
    * updated farm bot deploy to have reserve address
    
    * removed fixme
    
    * improved MockRouter (actually mints/burns LPs when you call add/remove liquidity methods)
    
    Co-authored-by: Joseph Bergeron <jophish126@gmail.com>

---
+++ contracts/mocks/MockRouter.sol
@@ -0,0 +1,102 @@
+pragma solidity ^0.8.0;
+
+import "../ubeswap/contracts/uniswapv2/interfaces/IUniswapV2Router01.sol";
+import "./MockLPToken.sol";
+
+contract MockRouter is IUniswapV2Router01 {
+    MockLPToken lpToken;
+    constructor(address _lpToken) {
+        lpToken = MockLPToken(_lpToken);
+    }
+
+    function factory() external override pure returns (address) {
+        require(false, "Shouldn't use factory for mock router");
+        return address(0);
+    }
+    uint mockLiquidity;
+    function setMockLiquidity(uint _liquidity) public {
+        mockLiquidity = _liquidity;
+    }
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external override returns (uint amountA, uint amountB, uint liquidity) {
+        amountA = amountADesired;
+        amountB = amountBDesired;
+        liquidity = mockLiquidity;
+        lpToken.mint(msg.sender, mockLiquidity);
+    }
+    function removeLiquidity(
+        address tokenA,
+        address tokenB,
+        uint liquidity,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external override returns (uint amountA, uint amountB) {
+        amountA = amountAMin;
+        amountB = amountBMin;
+        lpToken.burn(liquidity);
+    }
+    function removeLiquidityWithPermit(
+        address tokenA,
+        address tokenB,
+        uint liquidity,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline,
+        bool approveMax, uint8 v, bytes32 r, bytes32 s
+    ) external override returns (uint amountA, uint amountB) {
+        amountA = amountAMin;
+        amountB = amountBMin;
+    }
+    uint[] mockAmounts;
+    function setMockAmounts(uint[] memory amounts) public {
+        mockAmounts = new uint[](amounts.length);
+        for (uint idx = 0; idx < amounts.length; idx++) {
+            mockAmounts[idx] = amounts[idx];
+        }
+    }
+    function swapExactTokensForTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external override returns (uint[] memory amounts) {
+        return mockAmounts;
+    }
+    function swapTokensForExactTokens(
+        uint amountOut,
+        uint amountInMax,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external override returns (uint[] memory amounts) {
+        return mockAmounts;
+    }
+
+    function quote(uint amountA, uint reserveA, uint reserveB) external override pure returns (uint amountB){
+        amountB = reserveB / reserveA * amountA;
+    }
+    function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) external override pure returns (uint amountOut){
+        return reserveOut / reserveIn * amountIn;
+    }
+    function getAmountIn(uint amountOut, uint reserveIn, uint reserveOut) external override pure returns (uint amountIn){
+        return reserveIn / reserveOut * amountOut;
+    }
+    function getAmountsOut(uint amountIn, address[] calldata path) external override view returns (uint[] memory amounts){
+        return mockAmounts;
+    }
+    function getAmountsIn(uint amountOut, address[] calldata path) external override view returns (uint[] memory amounts){
+        return mockAmounts;
+    }
+}
---
+++ test/farm-bot-test.ts
@@ -0,0 +1,71 @@
+import {expect} from "chai"
+import {FarmBot, MockERC20, MockLPToken, MockRevoBounty, MockRouter, MockMoolaStakingRewards} from "../typechain";
+
+const {ethers} = require("hardhat")
+
+
+describe('Farm bot tests', () => {
+  it('Able to deploy farm bot to local test chain', async () => {
+    const [owner, reserve] = await ethers.getSigners()
+    const revoBountyFactory = await ethers.getContractFactory('MockRevoBounty')
+    const bountyContract: MockRevoBounty = await revoBountyFactory.deploy()
+
+    const erc20Factory = await ethers.getContractFactory('MockERC20')
+
+    const rewardsToken0Contract: MockERC20 = await erc20Factory.deploy('Mock rewards token 0', 'RWD0')
+    const rewardsToken1Contract: MockERC20 = await erc20Factory.deploy('Mock rewards token 1', 'RWD1')
+    const rewardsToken2Contract: MockERC20 = await erc20Factory.deploy('Mock rewards token 2', 'RWD2')
+
+    const token0Contract: MockERC20 = await erc20Factory.deploy('Mock token 0', 'T0')
+    const token1Contract: MockERC20 = await erc20Factory.deploy('Mock token 1', 'T1')
+
+    const lpFactory = await ethers.getContractFactory('MockLPToken')
+    const lpTokenContract: MockLPToken = await lpFactory.deploy(
+      'Mock staking token', // name
+      'LP', // symbol
+      token0Contract.address,
+      token1Contract.address
+    )
+
+    const stakingRewardsFactory = await ethers.getContractFactory('MockMoolaStakingRewards')
+    const stakingRewardsContract: MockMoolaStakingRewards = await stakingRewardsFactory.deploy(
+      token0Contract.address, // rewards token
+      [rewardsToken0Contract.address, rewardsToken1Contract.address, rewardsToken2Contract.address],
+      lpTokenContract.address
+    )
+
+    const routerFactory = await ethers.getContractFactory('MockRouter')
+    const routerContract: MockRouter = await routerFactory.deploy(lpTokenContract.address)
+
+    // sanity check mock LP contract
+    const stakingToken0 = await lpTokenContract.token0()
+    expect(stakingToken0).to.equal(token0Contract.address)
+    const stakingToken1 = await lpTokenContract.token1()
+    expect(stakingToken1).to.equal(token1Contract.address)
+
+    // sanity check rewards
+    expect(await stakingRewardsContract.rewardsToken())
+      .to.equal(token0Contract.address)
+    expect(await stakingRewardsContract.stakingToken())
+      .to.equal(lpTokenContract.address)
+
+    const farmBotFactory = await ethers.getContractFactory('FarmBot')
+    const farmBotContract: FarmBot = await farmBotFactory.deploy(
+      owner.address,
+      reserve.address,
+      stakingRewardsContract.address,
+      lpTokenContract.address,
+      bountyContract.address,
+      routerContract.address,
+      [rewardsToken0Contract.address, rewardsToken1Contract.address, rewardsToken2Contract.address],
+      [
+	[[],[]],
+	[[],[]],
+	[[],[]]
+      ],
+      'FP'
+    )
+
+    expect(!!farmBotContract.address).not.to.be.false
+  })
+})
