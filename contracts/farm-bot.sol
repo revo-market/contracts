0d615d45c76e681d38facc5f4a3ff6401e155648
Joseph Bergeron <jophish126@gmail.com>
2022-02-13 18:23:53 -0500

    fix naming

--- contracts/farm-bot.sol
+++ contracts/farm-bot.sol
@@ -254,8 +254,8 @@ contract FarmBot is ERC20, AccessControl {
     {
         TokenAmount[] memory _rewardsTokenBalances = calculateRewards();
 
-        compounderFee = revoBounty.compounderFee(_rewardsTokenBalances);
-        compounderBonus = revoBounty.compounderBonus(_rewardsTokenBalances);
+        compounderFee = revoFees.compounderFee(_rewardsTokenBalances);
+        compounderBonus = revoFees.compounderBonus(_rewardsTokenBalances);
     }
 
     // Figure out best-case scenario amount of token we can get and swap
