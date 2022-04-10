7143899c8db54631b9f5d18d1bc5adcd79b9ddd6
Charlie Andrews <charlie.andrews@valoraapp.com>
2022-04-10 12:57:22 +0200

    fix and formatting for broker contract

--- contracts/fp-broker/RevoFPBroker.sol
+++ contracts/fp-broker/RevoFPBroker.sol
@@ -11,21 +11,19 @@ struct LiquidityAmounts {
 }
 
 /**
-* RevoFPBroker helps users get FP directly from staking tokens (skipping the step where you get LP first).
-*/
+ * RevoFPBroker helps users get FP directly from staking tokens (skipping the step where you get LP first).
+ */
 contract RevoFPBroker is Pausable, AccessControl {
     using SafeERC20 for IERC20;
 
     event RFPBrokerDeposit(
-      address indexed farmBotAddress,
-      address indexed depositorAddress,
-      uint256 lpGained,
-      uint256 fpGained
+        address indexed farmBotAddress,
+        address indexed depositorAddress,
+        uint256 lpGained,
+        uint256 fpGained
     );
 
-    constructor(
-        address _owner
-    ) {
+    constructor(address _owner) {
         _setupRole(DEFAULT_ADMIN_ROLE, _owner);
     }
 
@@ -43,51 +41,76 @@ contract RevoFPBroker is Pausable, AccessControl {
     }
 
     /**
-    * Get LP and deposit them into a farm bot.
-    *
-    * Works only for farm bots implementing RevoUniswapStakingTokenStrategy.
-    *
-    * @param _farmBotAddress: address of RevoUniswapStakingTokenStrategy contract
-    * @param _liquidityAmounts: how much liquidity to add (grouped to avoid 'stack too deep' error)
-    * @param _deadline: deadline to finish transaction by, in epoch seconds
-    */
+     * Get LP and deposit them into a farm bot.
+     *
+     * Works only for farm bots implementing RevoUniswapStakingTokenStrategy.
+     *
+     * @param _farmBotAddress: address of RevoUniswapStakingTokenStrategy contract
+     * @param _liquidityAmounts: how much liquidity to add (grouped to avoid 'stack too deep' error)
+     * @param _deadline: deadline to finish transaction by, in epoch seconds
+     */
     function getUniswapLPAndDeposit(
         address _farmBotAddress,
         LiquidityAmounts calldata _liquidityAmounts,
         uint256 _deadline
     ) external ensure(_deadline) whenNotPaused {
-        RevoUniswapStakingTokenStrategy _farmBot = RevoUniswapStakingTokenStrategy(_farmBotAddress);
+        RevoUniswapStakingTokenStrategy _farmBot = RevoUniswapStakingTokenStrategy(
+                _farmBotAddress
+            );
 
         // take staking tokens from sender
-        require(_liquidityAmounts.amount0Desired > 0, "amount0Desired must be greater than 0");
-        require(_liquidityAmounts.amount1Desired > 0, "amount1Desired must be greater than 0");
-        _farmBot.stakingToken0().safeTransferFrom(msg.sender, address(this), _liquidityAmounts.amount0Desired);
-        _farmBot.stakingToken1().safeTransferFrom(msg.sender, address(this), _liquidityAmounts.amount1Desired);
-
-        // add liquidity
-        (uint256 _amount0Invested, uint256 _amount1Invested, uint256 _lpAmount) = UniswapRouter.addLiquidity(
-            _farmBot.liquidityRouter(),
-            _farmBot.stakingToken0(),
-            _farmBot.stakingToken1(),
-            _liquidityAmounts.amount0Desired,
-            _liquidityAmounts.amount1Desired,
-            _liquidityAmounts.amount0Min,
-            _liquidityAmounts.amount1Min,
-            _deadline
+        require(
+            _liquidityAmounts.amount0Desired > 0,
+            "amount0Desired must be greater than 0"
+        );
+        require(
+            _liquidityAmounts.amount1Desired > 0,
+            "amount1Desired must be greater than 0"
+        );
+        _farmBot.stakingToken0().safeTransferFrom(
+            msg.sender,
+            address(this),
+            _liquidityAmounts.amount0Desired
+        );
+        _farmBot.stakingToken1().safeTransferFrom(
+            msg.sender,
+            address(this),
+            _liquidityAmounts.amount1Desired
         );
 
+        // add liquidity
+        (
+            uint256 _amount0Invested,
+            uint256 _amount1Invested,
+            uint256 _lpAmount
+        ) = UniswapRouter.addLiquidity(
+                _farmBot.liquidityRouter(),
+                _farmBot.stakingToken0(),
+                _farmBot.stakingToken1(),
+                _liquidityAmounts.amount0Desired,
+                _liquidityAmounts.amount1Desired,
+                _liquidityAmounts.amount0Min,
+                _liquidityAmounts.amount1Min,
+                _deadline
+            );
+
         // send leftovers to investor
-        uint256 _token0Leftover = _liquidityAmounts.amount0Desired - _amount0Invested;
+        uint256 _token0Leftover = _liquidityAmounts.amount0Desired -
+            _amount0Invested;
         if (_token0Leftover > 0) {
             _farmBot.stakingToken0().safeTransfer(msg.sender, _token0Leftover);
         }
-        uint256 _token1Leftover = _liquidityAmounts.amount1Desired - _amount1Invested;
+        uint256 _token1Leftover = _liquidityAmounts.amount1Desired -
+            _amount1Invested;
         if (_token1Leftover > 0) {
             _farmBot.stakingToken1().safeTransfer(msg.sender, _token1Leftover);
         }
 
         // trade LP for FP
-        _farmBot.stakingToken().safeIncreaseAllowance(_farmBotAddress, _lpAmount);
+        _farmBot.stakingToken().safeIncreaseAllowance(
+            _farmBotAddress,
+            _lpAmount
+        );
         uint256 _prevFPBalance = _farmBot.balanceOf(address(this));
         _farmBot.deposit(_lpAmount);
         uint256 _fpGained = _farmBot.balanceOf(address(this)) - _prevFPBalance;
@@ -103,14 +126,14 @@ contract RevoFPBroker is Pausable, AccessControl {
     }
 
     /**
-    * Withdraw from a farm bot and remove liquidity from the underlying pool.
-    *
-    * @param _farmBotAddress: address of the farm bot to withdraw from
-    * @param _fpAmount: amount to withdraw
-    * @param _amountAMin: minimum amount of staking token 0 (aka "token A") to receive in exchange
-    * @param _amountBMin: minimum amount of staking token 1 (aka "token B") to receive in exchange
-    * @param _deadline: time to finish by, in epoch seconds
-    */
+     * Withdraw from a farm bot and remove liquidity from the underlying pool.
+     *
+     * @param _farmBotAddress: address of the farm bot to withdraw from
+     * @param _fpAmount: amount to withdraw
+     * @param _amountAMin: minimum amount of staking token 0 (aka "token A") to receive in exchange
+     * @param _amountBMin: minimum amount of staking token 1 (aka "token B") to receive in exchange
+     * @param _deadline: time to finish by, in epoch seconds
+     */
     function withdrawFPForStakingTokens(
         address _farmBotAddress,
         uint256 _fpAmount,
@@ -119,20 +142,31 @@ contract RevoFPBroker is Pausable, AccessControl {
         uint256 _deadline
     ) external ensure(_deadline) whenNotPaused {
         require(_fpAmount > 0, "Cannot withdraw because _fpAmount is 0");
-        IERC20(_farmBotAddress).safeTransferFrom(msg.sender, address(this), _fpAmount);
-        RevoUniswapStakingTokenStrategy _farmBot = RevoUniswapStakingTokenStrategy(_farmBotAddress);
+        IERC20(_farmBotAddress).safeTransferFrom(
+            msg.sender,
+            address(this),
+            _fpAmount
+        );
+        RevoUniswapStakingTokenStrategy _farmBot = RevoUniswapStakingTokenStrategy(
+                _farmBotAddress
+            );
         uint256 _lpAmount = _farmBot.getLpAmount(_fpAmount);
+        uint256 _lpStartBalance = _farmBot.stakingToken().balanceOf(
+            address(this)
+        ); // should be 0 but just in case
         _farmBot.withdraw(_lpAmount);
-        (uint256 _amountA, uint256 _amountB) = _farmBot.liquidityRouter().removeLiquidity(
+        _farmBot.stakingToken().safeIncreaseAllowance(
+            address(_farmBot.liquidityRouter()),
+            _lpAmount
+        );
+        _farmBot.liquidityRouter().removeLiquidity(
             address(_farmBot.stakingToken0()),
             address(_farmBot.stakingToken1()),
-            _lpAmount,
+            _farmBot.stakingToken().balanceOf(address(this)) - _lpStartBalance, // can't just use _lpAmount because there is a withdrawal fee
             _amountAMin,
             _amountBMin,
-            address(this),
+            msg.sender,
             _deadline
         );
-        _farmBot.stakingToken0().safeTransfer(msg.sender, _amountA);
-        _farmBot.stakingToken1().safeTransfer(msg.sender, _amountB);
     }
 }
